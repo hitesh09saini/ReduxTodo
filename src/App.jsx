f57cc1513292c63863cd02a10d3918058e6cc409
Hitesh Saini <hitesh9694saini@gmail.com>
2023-10-30 00:01:30 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from "./components/AddTodo"
+import Todo from './components/Todo'
+
+function App() {
+ 
+  return (
+    <div className="p-4"> 
+     <AddTodo/>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
